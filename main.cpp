fe492017db7d8918c26a5ef5b38d2bc371e4ec0d
Ultras <UltrasMiro@gmail.com>
2025-10-29 19:44:07 +0200

    added binary tree

--- main.cpp
+++ main.cpp
@@ -1,84 +1,97 @@
 #include "vector.h"
-
 using namespace std;
 
 int main() {
-    vector<Vector> vec;
+    vector<Vector> vec;   // generic collection
+    list<Vector> lst;     // non-generic collection
+    Vector arr[3];        // array
+
     int choice;
     bool running = true;
 
     while (running) {
         cout << "\n=== VECTOR OPERATIONS MENU ===\n";
-        cout << "1. Add vector\n";
-        cout << "2. Delete vector\n";
-        cout << "3. Update vector\n";
-        cout << "4. Search vector by length\n";
-        cout << "5. Display all vectors\n";
-        cout << "6. Exit\n";
+        cout << "1. Add new vector (root)\n";
+        cout << "2. Show all vectors\n";
+        cout << "3. Add new list vector (left)\n";
+        cout << "4. Show all list vectors\n";
+        cout << "5. Add new array vector (right)\n";
+        cout << "6. Show all array vectors\n";
+        cout << "7. Show binary tree (Preorder)\n";
+        cout << "0. Exit\n";
         cin >> choice;
 
         switch (choice) {
             case 1: {
                 double x, y, z;
-                cout << "Enter coordinates (x y z):\n";
+                cout << "Enter coordinates (x y z): ";
                 cin >> x >> y >> z;
                 vec.push_back(Vector(x, y, z));
-                cout << "Vector added successfully.\n";
+                cout << "Vector has been added.\n";
                 break;
             }
 
             case 2: {
-                int index;
+                sort(vec.begin(), vec.end());
+                cout << "Sorted by length:\n";
                 print_all(vec);
-                cout << "Enter element number to delete:\n";
-                cin >> index;
-                if (index > 0 && index <= (int)vec.size()) {
-                    vec.erase(vec.begin() + index - 1);
-                    cout << "Vector deleted.\n";
-                } else cout << "Invalid index.\n";
                 break;
             }
 
             case 3: {
-                int index;
-                print_all(vec);
-                cout << "Enter element number to update:\n";
-                cin >> index;
-                if (index > 0 && index <= (int)vec.size()) {
-                    double x, y, z;
-                    cout << "Enter new coordinates (x y z): ";
-                    cin >> x >> y >> z;
-                    vec[index - 1].update(x, y, z);
-                    cout << "Vector updated.\n";
-                } else cout << "Invalid index.\n";
+                double x, y, z;
+                cout << "Enter coordinates (x y z): ";
+                cin >> x >> y >> z;
+                lst.push_back(Vector(x, y, z));
+                cout << "Vector added to std::list.\n";
                 break;
             }
 
             case 4: {
-                double target;
-                cout << "Enter minimum length for search:\n";
-                cin >> target;
-
-                bool found = false;
-                cout << "Vectors with length >= " << target << ":\n";
-                for (const auto& v : vec) {
-                    if (v.length() >= target) {
-                        v.print();
-                        cout << " | length = " << v.length() << endl;
-                        found = true;
-                    }
-                }
-
-                if (!found) {
-                    cout << "No vectors with length >= " << target << " found.\n";
+                cout << "List traversal:\n";
+                for (auto& v : lst) {
+                    v.print(); cout << " | length = " << v.length() << endl;
                 }
                 break;
             }
-            case 5:
-                print_all(vec);
-                break;
 
-            case 6:
+            case 5: {
+                cout << "Enter 3 vectors (x y z):\n";
+                for (int i = 0; i < 3; i++) {
+                    double x, y, z;
+                    cin >> x >> y >> z;
+                    arr[i] = Vector(x, y, z);
+                }
+                break;
+            }
+
+            case 6: {
+                cout << "Array traversal:\n";
+                for (auto& v : arr) {
+                    v.print(); cout << " | length = " << v.length() << endl;
+                }
+                break;
+            }
+
+            case 7: {
+                BinaryTree<Vector> tree;
+
+                for (auto& v : vec) tree.insert(v);
+                for (auto& v : lst) tree.insert(v);
+                for (auto& v : arr) tree.insert(v);
+
+                cout << "\nPreorder traversal using method:\n";
+                tree.preorder();
+
+                cout << "\nPreorder traversal using iterator:\n";
+                for (auto& v : tree) {
+                    v.print();
+                    cout << " | length = " << v.length() << endl;
+                }
+                break;
+            }
+
+            case 0:
                 running = false;
                 cout << "Exiting program.\n";
                 break;
@@ -87,5 +100,6 @@ int main() {
                 cout << "Unknown command.\n";
         }
     }
+
     return 0;
 }
--- vector.h
+++ vector.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <list>
 #include <cmath>
 #include <algorithm>
+#include <stack>
 using namespace std;
 
 class Vector {
@@ -41,3 +43,56 @@ void print_all(const vector<Vector>& vec) {
         cout << " | length = " << vec[i].length() << endl;
     }
 }
+
+template <typename T>
+class BinaryTree {
+private:
+    struct Node {
+        T data;
+        Node* left;
+        Node* right;
+        Node(const T& val) : data(val), left(nullptr), right(nullptr) {}
+    };
+    Node* root;
+
+    void insert(Node*& node, const T& val) {
+        if (!node) node = new Node(val);
+        else if (val < node->data) insert(node->left, val);
+        else insert(node->right, val);
+    }
+
+    void preorder(Node* node, int depth) const {
+        if (!node) return;
+        cout << string(depth * 4, ' ');
+        node->data.print();
+        cout << " | length = " << node->data.length() << endl;
+        preorder(node->left, depth + 1);
+        preorder(node->right, depth + 1);
+    }
+
+public:
+    BinaryTree() : root(nullptr) {}
+
+    void insert(const T& val) { insert(root, val); }
+
+    void preorder() const { preorder(root, 0); }
+
+    class Iterator {
+    private:
+        stack<Node*> nodes;
+    public:
+        Iterator(Node* root) { if (root) nodes.push(root); }
+        bool operator!=(const Iterator& other) const { return nodes != other.nodes; }
+        T& operator*() { return nodes.top()->data; }
+        Iterator& operator++() {
+            Node* curr = nodes.top();
+            nodes.pop();
+            if (curr->right) nodes.push(curr->right);
+            if (curr->left) nodes.push(curr->left);
+            return *this;
+        }
+    };
+
+    Iterator begin() const { return Iterator(root); }
+    Iterator end() const { return Iterator(nullptr); }
+};
